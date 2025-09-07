822a3cf64ff96242deac3bf2c1a1168ca3075b31
work <work.official.info.01@gmail.com>
2025-09-08 02:31:47 +0600

    mongo model add

---
+++ models/adminModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePicture: {
+      type: String, // Cloudinary/S3 URL or path
+      required: false,
+    },
+    resume: {
+      type: String, // URL of uploaded resume (PDF, DOCX, etc.)
+      required: false,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export const Admin =
+  mongoose.models.Admin || mongoose.model("Admin", adminSchema);
