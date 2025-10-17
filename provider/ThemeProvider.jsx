95698fc6ba84ada4578816e75f73c2eb3d8769b7
work <work.official.info.01@gmail.com>
2025-10-17 16:25:10 +0600

    initial theme selected dark

--- provider/ThemeProvider.jsx
+++ provider/ThemeProvider.jsx
@@ -4,12 +4,14 @@ import { ThemeContext } from "@/contex";
 import { useEffect, useState } from "react";
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark"); // default dark
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
-    setTheme(savedTheme);
-    document.documentElement.classList.toggle("dark", savedTheme === "dark");
+    const savedTheme = localStorage.getItem("theme");
+    const initialTheme = savedTheme || "dark"; // ✅ default dark if nothing saved
+
+    setTheme(initialTheme);
+    document.documentElement.classList.toggle("dark", initialTheme === "dark");
   }, []);
 
   const toggleTheme = () => {
