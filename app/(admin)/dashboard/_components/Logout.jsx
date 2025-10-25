58a053a69b54db8aee18c8bbc27c1ffdfe06d2e7
Tanvir <tanvir.info07@gmail.com>
2025-10-26 03:43:54 +0600

    fox logout callback uri

--- app/(admin)/dashboard/_components/Logout.jsx
+++ app/(admin)/dashboard/_components/Logout.jsx
@@ -3,14 +3,9 @@ import { signOut } from "next-auth/react";
 import React from "react";
 
 const Logout = () => {
-  const handleLogout = () => {
-    // Use the current origin to redirect properly
-    signOut({ callbackUrl: window.location.origin + "/admin-login" });
-  };
-
   return (
     <button
-      onClick={handleLogout}
+      onClick={() => signOut({ callbackUrl: "/admin-login" })}
       className="bg-gradient-to-r w-full from-pink-500 to-red-600 hover:from-pink-600 hover:to-red-700 text-white p-2 rounded-lg cursor-pointer"
     >
       Logout
